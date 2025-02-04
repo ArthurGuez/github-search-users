81d259469815e51bfc66de1eeb1d9e70b72a6a94
Arthur Guez <arthurguez29@gmail.com>
2025-02-04 16:11:07 +0100

    feat(errors): add ErrorBoundary component

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import type { ReactNode } from "react";
+import React from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <>Oh no! An error has occured</>;
+    }
+
+    return this.props.children;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import EditModeProvider from "./contexts/edit-mode/EditModeProvider.tsx";
+import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <EditModeProvider>
-      <App />
-    </EditModeProvider>
+    <ErrorBoundary>
+      <EditModeProvider>
+        <App />
+      </EditModeProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
